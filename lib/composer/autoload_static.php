1eb69a7afc45803af55f977d9e0a1b0df37b966f
herminio-git <git.herminio@gmail.com>
2019-09-12 15:20:05 -0300

    Adicioando PhpMailer

--- lib/composer/autoload_static.php
+++ lib/composer/autoload_static.php
@@ -10,9 +10,25 @@ class ComposerStaticInit842521274775389227ee3d1cba0aa583
         'f084d01b0a599f67676cffef638aa95b' => __DIR__ . '/..' . '/smarty/smarty/libs/bootstrap.php',
     );
 
+    public static $prefixLengthsPsr4 = array (
+        'P' => 
+        array (
+            'PHPMailer\\PHPMailer\\' => 20,
+        ),
+    );
+
+    public static $prefixDirsPsr4 = array (
+        'PHPMailer\\PHPMailer\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/phpmailer/phpmailer/src',
+        ),
+    );
+
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
+            $loader->prefixLengthsPsr4 = ComposerStaticInit842521274775389227ee3d1cba0aa583::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInit842521274775389227ee3d1cba0aa583::$prefixDirsPsr4;
 
         }, null, ClassLoader::class);
     }
