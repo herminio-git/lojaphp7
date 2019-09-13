be1a7b600afcb9e4305bcee787a8aa479f4e3453
herminio-git <git.herminio@gmail.com>
2019-09-13 10:52:41 -0300

    Constantes para o site

---
+++ model/Config.class.php
@@ -0,0 +1,8 @@
+<?php 
+
+    const SITE_URL = "http://localhost";
+    const SITE_PASTA = "loja";
+    const SITE_NOME = "Ecommerce - PHP7";
+    const SITE_EMAIL_ADM = "git.herminio@gmail.com";
+
+?>
