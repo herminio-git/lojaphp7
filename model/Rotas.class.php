a8664fc64dae9f558d568304628b3fae716a7e90
herminio-git <git.herminio@gmail.com>
2019-09-13 07:40:32 -0300

    Usando (explode) para url

--- model/Rotas.class.php
+++ model/Rotas.class.php
@@ -2,10 +2,21 @@
 
     Class Rotas{
 
+        public static $pag;
+
         static function get_Pagina(){
             if(isset($_GET['pag'])){
-                $pagina = 'controller/' .$_GET['pag'].'.php';
-                
+
+                // Variável "$pagina" recebe "$_GET" (digitado/linkado na url)
+                $pagina = $_GET['pag'];
+
+                // "self::" se equivale a "$this"
+                self::$pag = explode('/', $pagina);
+
+                // Faz a verificação se existe a página na pasta controller
+                $pagina = 'controller/' . self::$pag[0] .'.php';
+                // $pagina = 'controller/' .$_GET['pag'].'.php';
+
                 if(file_exists($pagina)){
                     include $pagina;
 
