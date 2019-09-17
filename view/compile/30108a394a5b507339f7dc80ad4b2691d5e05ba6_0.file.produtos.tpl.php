e1651a676a5391560b588b94846fc9dad374724c
herminio-git <git.herminio@gmail.com>
2019-09-17 20:14:43 -0300

    shopping fotos

--- view/compile/30108a394a5b507339f7dc80ad4b2691d5e05ba6_0.file.produtos.tpl.php
+++ view/compile/30108a394a5b507339f7dc80ad4b2691d5e05ba6_0.file.produtos.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.33, created on 2019-09-17 23:55:41
+/* Smarty version 3.1.33, created on 2019-09-18 00:01:53
   from 'C:\xampp\htdocs\qcursos\lojaphp7\view\produtos.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.33',
-  'unifunc' => 'content_5d81565d07ec38_64597847',
+  'unifunc' => 'content_5d8157d1527523_22325922',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '30108a394a5b507339f7dc80ad4b2691d5e05ba6' => 
     array (
       0 => 'C:\\xampp\\htdocs\\qcursos\\lojaphp7\\view\\produtos.tpl',
-      1 => 1568757258,
+      1 => 1568757707,
       2 => 'file',
     ),
   ),
@@ -20,6 +20,178 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_5d81565d07ec38_64597847 (Smarty_Internal_Template $_smarty_tpl) {
-?><h2>CONTROLLER/PRODUTOS.PHP</h2> <br><?php }
+function content_5d8157d1527523_22325922 (Smarty_Internal_Template $_smarty_tpl) {
+?><h3>Lista de produtos</h3>
+<hr>
+
+ 
+    <section id="pagincao" class="row">
+       <center>
+          PAGINAS
+       </center>
+    </section>
+  
+       
+    <!--  começa lista de produtos  ---->   
+  <section id="produtos" class="row">  
+ 
+<ul style="list-style: none" >
+    
+    
+  
+                  <div class="row" id="pularliha">
+             
+           
+        <li class="col-md-4">
+
+            <div class="thumbnail">
+
+                <a href="">
+
+
+                    <img src="" alt="" > 
+
+                    <div class="caption">
+
+                        <h4 class="text-center"> NOME PROD</h4> 
+
+                        <h3 class="text-center text-danger">VALOR</h3>
+
+                    </div>
+
+                </a>
+
+            </div>
+
+
+        </li>
+        
+         </div>
+         
+    
+</ul>
+    
+    </section>
+    
+    
+     <!--  paginação inferior   -->  
+    <section id="pagincao" class="row">
+    <center>
+    PAGINAS
+    </center>
+    </section>
+
+
+
+
+
+
+
+
+   // PRODUTOS INFO PAGINA
+
+
+     <?php
+$_from = $_smarty_tpl->smarty->ext->_foreach->init($_smarty_tpl, $_smarty_tpl->tpl_vars['PRO']->value, 'P');
+if ($_from !== null) {
+foreach ($_from as $_smarty_tpl->tpl_vars['P']->value) {
+?>
+
+      <h3 class="text-center"><?php echo $_smarty_tpl->tpl_vars['P']->value['pro_nome'];?>
+</h3>
+<hr>
+
+<div class="row">
+    
+  
+        <div class="col-md-6 ">
+
+        <img class="thumbnail" src="<?php echo $_smarty_tpl->tpl_vars['P']->value['pro_img'];?>
+" alt="" > 
+
+    </div>
+
+   
+	    <div class="col-md-6 thumbnail">
+        
+          <img src="<?php echo $_smarty_tpl->tpl_vars['TEMA']->value;?>
+/images/logo-pagseguro.png" alt="">
+  <hr>
+            
+        
+        <div class="col-md-6">
+           <h3 class="text-center preco">R$ <?php echo $_smarty_tpl->tpl_vars['P']->value['pro_valor'];?>
+</h3>   
+            
+        </div>
+      
+        <div class="col-md-6">
+            <form name="carrinho" method="post" action="">
+                <input type="hidden" name="pro_id" value="<?php echo $_smarty_tpl->tpl_vars['P']->value['pro_id'];?>
+">
+                <input type="hidden" name="acao" value="add">
+            <button  class="btn btn-geral btn-lg">Comprar</button>
+             </form> 
+            
+        </div>
+           
+         
+      
+
+     
+        
+    
+
+    </div>
+
+
+
+
+</div>
+        <!-- -->
+                 <div class="row">
+            <hr>
+              <h4 class="text-center">Mais imagens</h4>
+
+              <ul style="list-style: none">
+                 
+
+                      <li class="col-md-3 ">
+                          <img src="" alt="" class="thumbnail">
+
+
+                      </li>
+
+                 
+              </ul>
+              
+
+        </div>
+                  
+        <div class="row">
+            <hr>
+            <h4 class="text-center">Descrição do produto</h4>
+
+            <?php echo $_smarty_tpl->tpl_vars['P']->value['pro_desc'];?>
+ 
+
+        </div>  
+            <br>
+            <br>
+
+
+  <?php
+}
+}
+$_smarty_tpl->smarty->ext->_foreach->restore($_smarty_tpl, 1);?>
+
+
+
+
+
+
+
+
+
+<?php }
 }
